419513b12a02949315d7c2ada48e8862310fba28
demo <t.h.naveen1605@gmail.com>
2026-09-10 18:50:30 +0530

    Initial project setup

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,23 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/lucide-react')) {
+            return 'lucide-icons';
+          }
+          if (
+            id.includes('node_modules/react') ||
+            id.includes('node_modules/react-dom') ||
+            id.includes('node_modules/react-router-dom')
+          ) {
+            return 'react-vendor';
+          }
+        },
+      },
+    },
+  },
 })
